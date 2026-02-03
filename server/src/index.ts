2f3916e481bf78d58f6a45f77bd247a37437cf23
MarkDemidovs <markdemidovs@proton.me>
2026-02-03 19:54:43 +0200

    index.ts app.use usersRouter

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import usersRouter from "./routes/users";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const port = Number(process.env.PORT) || 4000;
+app.use("/users", usersRouter);
 
+const port = Number(process.env.PORT) || 4000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
