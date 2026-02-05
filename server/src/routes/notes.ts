6e678bb1e7e9ec0033784622652781dbb06d01ab
MarkDemidovs <markdemidovs@proton.me>
2026-02-05 22:42:51 +0200

    notes route titleRenameNote

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getNotes, createNote } from "../controllers/notes";
+import { getNotes, createNote, titleRenameNote } from "../controllers/notes";
 
 const router = Router();
 
 router.get("/", getNotes);
 router.post("/", createNote);
+router.patch("/:id", titleRenameNote)
 
 export default router;
